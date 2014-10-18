c8162b7456658ae4cdf272cf7e07c6090dab15e3
Isaac Mailach <isaac.mailach@gmail.com>
2014-10-18 16:35:07 -0400

    Commit 7
    Change loading function of item info popup. Add play buttons to items. Change play icon to pause icon on click. Add date added to items.

--- js/musical-compositions.js
+++ js/musical-compositions.js
@@ -2,11 +2,16 @@ $(document).ready(function () {
     Math.random();
     $.getJSON('data/musical-compositions.json', function (database) {
         for (var i = 0; i < database.item.length; i++) {
-            $('.page-content-grid').append('<div class="page-content-grid-item" data-id="' + database.item[i].id +'" style="flex-grow: ' + Math.random() + ';"><img class="page-content-grid-item-image" src="img/musical-compositions/' + database.item[i].id + '/image.jpg" /><div class="page-content-grid-item-overlay"><h3 class="page-content-grid-item-overlay-heading">' + database.item[i].name + '</h1></div></div>');
+            $('.page-content-grid').append('<div class="page-content-grid-item" data-id="' + database.item[i].id +'" style="flex-grow: ' + Math.random() + ';"><img class="page-content-grid-item-image" src="img/musical-compositions/' + database.item[i].id + '/image.jpg" /><div class="page-content-grid-item-overlay"><h3 class="page-content-grid-item-overlay-heading">' + database.item[i].name + '</h1><small class="page-content-grid-item-overlay-meta">' + database.item[i].date + '</small><div class="page-content-grid-item-overlay-player">&#xf04b;</div></div></div>');
         }
+        $('.page-content-grid-item-overlay-player').click(function () {
+            event.stopPropagation();
+            $(this).html('&#xf04c;');
+        });
         $('.page-content-grid-item').click(function () {
             OpenPopup($(this).data('id'));
         });
+        
     }).fail(function(){
         alert('Sorry! Could not load the webpage properly. Make sure you are connected to the Internet and then try refreshing the page.');
     });
@@ -24,16 +29,11 @@ $(document).ready(function () {
     
     
     function OpenPopup (id) {
-        $('.popup').css('display', 'block');
-        setTimeout(function () {$('.popup').addClass('open');}, 50);
-        $('.popup-content-image').attr('src', 'img/musical-compositions/' + id + '/cover.jpg');
-        $.getJSON('data/musical-compositions.json', function (database) {
-            var item = $.grep(database.item, function(e){ return e.id == id; });
-            $('.popup-content-body-heading').text(item[0].name);
-            $('.popup-content-body-meta-date').text(item[0].date);
-        });
         $.get('text/musical-compositions/' + id + '.txt', function (text) {
+            $('.popup-content-image').attr('src', 'img/musical-compositions/' + id + '/cover.jpg');
             $('.popup-content-body').append(marked(text));
+            $('.popup').css('display', 'block');
+            setTimeout(function () {$('.popup').addClass('open');}, 50);
         }, 'text').fail(function () {
             alert('Sorry! Could not load the webpage properly. Make sure you are connected to the Internet and then try refreshing the page.');
         });
