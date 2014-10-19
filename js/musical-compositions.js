cf54166fae330ca018bf14ad037a622d093413ca
Isaac Mailach <isaac.mailach@gmail.com>
2014-10-19 11:35:09 -0400

    Commit 8
    Remove player from items. Add query string to URL to store open popup state. Add composition recordings to file tree.

--- js/musical-compositions.js
+++ js/musical-compositions.js
@@ -1,18 +1,16 @@
 $(document).ready(function () {
-    Math.random();
+    if (window.location.search.substring(1)) {
+        var query = JSON.parse('{"' + decodeURI(window.location.search.substring(1)).replace(/&/g, '","').replace(/=/g,'":"') + '"}');
+        OpenPopup(query.id);
+    }
     $.getJSON('data/musical-compositions.json', function (database) {
         for (var i = 0; i < database.item.length; i++) {
-            $('.page-content-grid').append('<div class="page-content-grid-item" data-id="' + database.item[i].id +'" style="flex-grow: ' + Math.random() + ';"><img class="page-content-grid-item-image" src="img/musical-compositions/' + database.item[i].id + '/image.jpg" /><div class="page-content-grid-item-overlay"><h3 class="page-content-grid-item-overlay-heading">' + database.item[i].name + '</h1><small class="page-content-grid-item-overlay-meta">' + database.item[i].date + '</small><div class="page-content-grid-item-overlay-player">&#xf04b;</div></div></div>');
+            $('.page-content-grid').append('<div class="page-content-grid-item" data-id="' + database.item[i].id +'" style="flex-grow: ' + Math.random() + ';"><img class="page-content-grid-item-image" src="img/musical-compositions/' + database.item[i].id + '/image.jpg" /><div class="page-content-grid-item-overlay"><h3 class="page-content-grid-item-overlay-heading">' + database.item[i].name + '</h1><small class="page-content-grid-item-overlay-meta">' + database.item[i].date + '</small></div></div>');
         }
-        $('.page-content-grid-item-overlay-player').click(function () {
-            event.stopPropagation();
-            $(this).html('&#xf04c;');
-        });
         $('.page-content-grid-item').click(function () {
             OpenPopup($(this).data('id'));
         });
-        
-    }).fail(function(){
+    }).fail(function () {
         alert('Sorry! Could not load the webpage properly. Make sure you are connected to the Internet and then try refreshing the page.');
     });
     $(document).keydown(function (e) {
@@ -27,11 +25,11 @@ $(document).ready(function () {
         ClosePopup();
     });
     
-    
     function OpenPopup (id) {
         $.get('text/musical-compositions/' + id + '.txt', function (text) {
             $('.popup-content-image').attr('src', 'img/musical-compositions/' + id + '/cover.jpg');
-            $('.popup-content-body').append(marked(text));
+            $('.popup-content-body').html(marked(text));
+            window.history.replaceState('', 'Isaac Mailach - Musical Compositions', '/index.html?id=' + id);
             $('.popup').css('display', 'block');
             setTimeout(function () {$('.popup').addClass('open');}, 50);
         }, 'text').fail(function () {
@@ -39,6 +37,7 @@ $(document).ready(function () {
         });
     }
     function ClosePopup () {
+        window.history.replaceState('', 'Isaac Mailach - Musical Compositions', '/index.html');
         $('.popup').removeClass('open');
         setTimeout(function () {$('.popup').css('display', 'none');}, 1001);
     }
