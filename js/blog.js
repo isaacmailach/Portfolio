48e15c1b401c797cb9dab2e9be7f598125b033ff
Isaac Mailach <isaac.mailach@gmail.com>
2014-12-21 18:20:54 -0500

    Commit 25
    Load first three blog posts. Fix Blog search input focus effect. Add logo to footer. Fix search input appearance and layout.

--- js/blog.js
+++ js/blog.js
@@ -8,23 +8,37 @@ $(document).ready(function () {
         var vars = subString[i].split('=');
         query[vars[0]] = vars[1];
     }
+    var test = $.get('text/blog/0003.txt', function (text) {return text;});
     $.getJSON('data/blog.json', function (database) {
         for (var i = 0; i < database.item.length; i++) {
             item_data[database.item[i].id] = database.item[i];
-            $('.page-content-aside').append('<div class="page-content-grid-aside-item" data-id="' + database.item[i].id + '">' + database.item[i].name + '<time>' + database.item[i].date + '</time></div>');
+            $('.page-content-aside').append('<a class="page-content-aside-item" data-id="' + database.item[i].id + '">' + database.item[i].name + '<time>' + database.item[i].date + '</time></a>');
+            if (i < 3) {
+                $.get('text/blog/' + database.item[i].id + '.txt', function (text) {
+                    $('.page-content-articlebox').append('<article class="page-content-articlebox-article">' + marked(text) + '</article>');
+                });
+            }
         }
         //$('.page-content-articlebox').append('<article class="page-content-grid-item hide" data-id="' + database.item[i].id + '" style="flex: ' + Math.random() + ' 0 ' + (250 + (Math.random() * 250)) + 'px;"><img class="page-content-grid-item-image" src="img/musical-compositions/' + database.item[i].id + '/image.jpg" /><div class="page-content-grid-item-overlay"><h3 class="page-content-grid-item-overlay-heading">' + database.item[i].name + '</h1><small class="page-content-grid-item-overlay-meta">' + database.item[i].date + '</small></div></div>');
-        $('.page-content-search').removeClass('hide');
+        $('.page-content-aside-search').removeClass('hide');
         if (query.q) {
             SearchItems(query.q);
             $('.page-content-search-input').val(query.q).focus();
         }
-        $('.page-content-aside-search').removeClass('hide');
     }).fail(function () {
         alert('Sorry! Could not load the webpage properly. Make sure you are connected to the Internet and then try refreshing the page.');
     });
     
-    $('.page-content-search-input').keyup(function () {
+    $('.page-content-aside-search-icon').click(function () {
+        $('.page-content-aside-search-inputbox-input').focus();
+    });
+    $('.page-content-aside-search-inputbox-input').focus(function () {
+        $('.page-content-aside-search-icon').addClass('page-content-aside-search-icon_focus');
+    });
+    $('.page-content-aside-search-inputbox-input').focusout(function () {
+        $('.page-content-aside-search-icon').removeClass('page-content-aside-search-icon_focus');
+    });
+    $('.page-content-aside-search-inputbox-input').keyup(function () {
         SearchItems($(this).val());
         query.q = $(this).val();
         UpdateQueries();
