8fd2740f2663fc74c7ac764548c5dbb3667d8e22
Isaac Mailach <isaac.mailach@gmail.com>
2015-03-07 21:22:10 -0500

    Commit 27-2
    Add loading fade-in animation. Links in aside now shows related blog post.
    Read URL queries to load related blog post. Enable search feature in
    aside. Layout, animation, and design tweaks. Remove Markdown posts to load posts more
    efficiently. Delete test posts.

--- js/blog.js
+++ js/blog.js
@@ -2,28 +2,42 @@ $(document).ready(function () {
     var query = {};
     var subString = window.location.search.substring(1).split('&');
     var item_data = {};
-    var current_url = 'http%3A%2F%2Fisaacmailach.github.io%2FPortfolio%2Fblog.html';
+    var current_url = 'http%3A%2F%2Fisaacmailach.im%2Fblog.html';
     
     for (var i = 0; i < subString.length; i++) {
         var vars = subString[i].split('=');
         query[vars[0]] = vars[1];
     }
-    var test = $.get('text/blog/0003.txt', function (text) {return text;});
     $.getJSON('data/blog.json', function (database) {
         for (var i = 0; i < database.item.length; i++) {
-            item_data[database.item[i].id] = database.item[i];
-            $('.page-content-aside').append('<a class="page-content-aside-item" data-id="' + database.item[i].id + '">' + database.item[i].name + '<time>' + database.item[i].date + '</time></a>');
+            item_data[database.item[i].post] = database.item[i];
+            $('.page-content-aside').append('<a class="page-content-aside-item hide" data-post="' + database.item[i].post + '">' + database.item[i].name + '<time class="page-content-aside-item-date">' + database.item[i].date + '</time></a>');
             if (i < 3) {
-                $.get('text/blog/' + database.item[i].id + '.txt', function (text) {
-                    $('.page-content-articlebox').append('<article class="page-content-articlebox-article">' + marked(text) + '</article>');
+                $.get('text/blog/' + database.item[i].post + '.html', function (text) {
+                    $('.page-content-articlebox').append('<article class="page-content-articlebox-article hide">' + text + '</article>');
+                    var hidden_elements = $('.hide');
+                    var fade_in_delay = 75;
+                    var counter = 0;
+                    (function FadeIn () {
+                        if (counter == hidden_elements.length) return;
+                        setTimeout(function () {
+                            $(hidden_elements[counter]).removeClass('hide');
+                            counter++;
+                            FadeIn();
+                        }, fade_in_delay);
+                    })();
                 });
             }
         }
-        //$('.page-content-articlebox').append('<article class="page-content-grid-item hide" data-id="' + database.item[i].id + '" style="flex: ' + Math.random() + ' 0 ' + (250 + (Math.random() * 250)) + 'px;"><img class="page-content-grid-item-image" src="img/musical-compositions/' + database.item[i].id + '/image.jpg" /><div class="page-content-grid-item-overlay"><h3 class="page-content-grid-item-overlay-heading">' + database.item[i].name + '</h1><small class="page-content-grid-item-overlay-meta">' + database.item[i].date + '</small></div></div>');
-        $('.page-content-aside-search').removeClass('hide');
         if (query.q) {
             SearchItems(query.q);
             $('.page-content-search-input').val(query.q).focus();
+        }
+        $('.page-content-aside-item').click(function () {
+            OpenPost($(this).data('post'));
+        });
+        if (query.post) {
+            OpenPost(query.post);
         }
     }).fail(function () {
         alert('Sorry! Could not load the webpage properly. Make sure you are connected to the Internet and then try refreshing the page.');
@@ -40,7 +54,38 @@ $(document).ready(function () {
     });
     $('.page-content-aside-search-inputbox-input').keyup(function () {
         SearchItems($(this).val());
-        query.q = $(this).val();
-        UpdateQueries();
     });
+    function OpenPost (post) {
+        $('.page-content-articlebox-article').addClass('hide');
+        setTimeout(function () {
+            $('.page-content-articlebox').empty();
+            $.get('text/blog/' + post + '.html', function (text) {
+                $('.page-content-articlebox').html('<article class="page-content-articlebox-article hide">' + text + '</article>');
+                setTimeout(function () {$('.page-content-articlebox-article').removeClass('hide');}, 1);
+            });
+        }, 280);
+        query.post = post;
+        UpdateQueries();
+    }
+    function SearchItems (phrase) {
+        $('.page-content-aside-item').each(function () {
+            var item = this;
+            if ($(this).text().search(new RegExp(phrase, "i")) < 0) {
+                $(this).addClass('hide');
+                setTimeout(function () {$(item).css('display', 'none');}, 280);
+            } else {
+                $(this).css('display', 'block');
+                setTimeout(function () {$(item).removeClass('hide');}, 1);
+            }
+        });
+    }
+    function UpdateQueries () {
+        var queryString = '';
+        for (var prop in query) {
+            if (query[prop]) {
+                queryString += prop + '=' + query[prop] + '&';
+            }
+        }
+        window.history.replaceState('', 'Isaac Mailach - Blog', window.location.pathname + '?' + queryString);
+    }
 });
