7f0111ff4a090122e675933e97f45b071adb76ec
Isaac Mailach <isaac.mailach@gmail.com>
2016-07-20 18:26:56 -0400

    Commit 88
    Add hover info to more clickable elements. Allow user to tab to modal
    arrows. Extensive coding improvements pertaining to readability and
    efficiency.

--- js/musical-compositions.js
+++ js/musical-compositions.js
@@ -11,60 +11,45 @@ $(document).ready(function () {
     var previous_item = false;
     var current_id = null;
     var current_num = null;
+    var current_data = {};
     
     $.getJSON('data/musical-compositions.json', function (database) {
         item_data = database.item;
         var item_template = document.querySelector('.page-content-grid-item_template');
         for (var i = 0; i < database.item.length; i++) {
-            item_num[item_data[i].id] = i;
+            var temp_item_data = item_data[i];
+            item_num[temp_item_data.id] = i;
             var item = document.createElement('div');
             item.className = 'page-content-grid-item hide';
-            if (item_data[i].align_top) {item.className += ' align-top';}
+            if (temp_item_data.align_top) {item.className += ' align-top';}
             item.setAttribute('tabindex', 0);
             item.setAttribute('data-num', i);
-            if (item_data[i].id == '0010') {
+            if (temp_item_data.id == '0010') {
                 item.setAttribute('style', 'flex-grow: 150;');
             } else {
                 item.setAttribute('style', 'flex-grow: ' + parseInt(100 * Math.random()));
             }
             var template = document.importNode(item_template.content, true);
-            template.querySelector('.page-content-grid-item-image').src = 'img/musical-compositions/' + item_data[i].id + '/image.jpg';
-            template.querySelector('.page-content-grid-item-overlay-heading').innerText = item_data[i].name;
-            template.querySelector('.page-content-grid-item-overlay-meta').innerText = item_data[i].date;
+            template.querySelector('.page-content-grid-item-image').src = 'img/musical-compositions/' + temp_item_data.id + '/image.jpg';
+            template.querySelector('.page-content-grid-item-overlay-heading').innerText = temp_item_data.name;
+            template.querySelector('.page-content-grid-item-overlay-meta').innerText = temp_item_data.date;
             item.appendChild(template);
-            item.addEventListener('keyup', function (e) {
-                if (e.keyCode === 13) {
-                    current_num = $(this).data('num');
-                    query.id = item_data[current_num].id;
-                    UpdateQueries();
-                    if (popup_open) {
-                        ResetPopup();
-                    } else {
-                        OpenPopup();
-                    }
-                }
-            });
+            item.setAttribute('title', temp_item_data.name);
             item.addEventListener('keydown', function (e) {
                 if (!popup_open) {
                     if (e.keyCode === 37) {
-                        $(this).prev('.page-content-grid-item').focus();
+                        this.previousSibling.focus();
                     } else if (e.keyCode === 39) {
-                        $(this).next('.page-content-grid-item').focus();
+                        this.nextSibling.focus();
                     }
                 }
             });
             item.addEventListener('click', function () {
-                current_num = $(this).data('num');
-                query.id = item_data[current_num].id;
-                UpdateQueries();
-                OpenPopup();
+                SetModal(this.dataset.num);
             });
             $('.page-content-grid').append(item);
         }
         
-        window.onpopstate = function (event) {
-            CheckQueries();
-        }
         var counter = 0;
         (function FadeIn () {
             if (counter == database.item.length) return;
@@ -80,59 +65,44 @@ $(document).ready(function () {
         alert('Sorry! Could not load the webpage properly. Make sure you are connected to the Internet and then try refreshing the page.');
     });
     document.addEventListener('keydown', function (e) {
-        if (e.keyCode === 27 && popup_open) {
-            query.id = '';
-            UpdateQueries();
-            ClosePopup();
-        } else if (e.keyCode === 32 && popup_open) {
+        var key = e.keyCode;
+        if (key === 27 && popup_open) {
+            CloseModal();
+        } else if (key === 32 && popup_open) {
             ToggleAudio();
             e.preventDefault(); 
-        } else if (e.keyCode === 37 && popup_open && next_item) {
+        } else if (key === 37 && popup_open) {
             NextItem();
-        } else if (e.keyCode === 39 && popup_open && previous_item) {
+        } else if (key === 39 && popup_open) {
             PreviousItem();
+        } else if (key === 13) {
+            document.activeElement.click();
         }
     });
-    $('.modal-content').click(function () {
+    var modal = document.querySelector('.modal');
+    var modal_content = document.querySelector('.modal-content');
+    modal_content.addEventListener('click', function () {
         event.stopPropagation();
     });
-    $('.modal, .modal-content-header-close-icon').click(function () {
-        query.id = '';
-        UpdateQueries();
-        ClosePopup();
-    });
-    $('.modal-content-header-close').bind('keyup', function(e) {
-        if (e.keyCode === 13) {
-            query.id = '';
-            UpdateQueries();
-            ClosePopup();
-        }
-    });
+    modal.addEventListener('click', CloseModal);
+    modal_content.querySelector('.modal-content-header-close').addEventListener('click', CloseModal);
     
-    $('.page-content-search-icon').click(function () {
-        $('.page-content-search-input').focus();
+    var search_input = document.querySelector('.page-content-search-input');
+    document.querySelector('.page-content-search-icon').addEventListener('click', function () {
+        search_input.focus();
     });
-    $(document).scroll(function (e) {
-        e.preventDefault();
+    search_input.addEventListener('keyup', function () {
+        var TypedSearch = _.debounce(function (query) {
+            SearchItems(query);
+        }, 300);
+        TypedSearch(this.value);
     });
-
-    document.querySelector('.page-content-search-input').addEventListener('keyup', function () {
-        query.q = $(this).val();
-        SearchItems_debounce($(this).val());
-    });
-    $('.page-content-search-input').on('search', function () {
-        query.q = $(this).val();
-        UpdateQueries();
-        SearchItems($(this).val());
+    search_input.addEventListener('search', function () {
+        SearchItems(this.value);
     });
     
     var play_button = document.querySelector('.modal-content-header-toolbar-player-play');
-    play_button.addEventListener('click', function () {ToggleAudio();});
-    play_button.addEventListener('keyup', function(e) {
-        if (e.keyCode === 13) {
-            ToggleAudio();
-        }
-    });
+    play_button.addEventListener('click', ToggleAudio);
     document.querySelector('.modal-arrow_left').addEventListener('click', function () {
         if (popup_open && next_item) {
             NextItem();
@@ -145,12 +115,12 @@ $(document).ready(function () {
         }
         event.stopPropagation();
     });
-    $('.modal-content').on("swiperight", function () {
+    $('.modal-content').on('swiperight', function () {
         if (popup_open && next_item && document.documentElement.clientWidth <= 700) {
             NextItem();
         }
     });
-    $('.modal').on("swipeleft", function () {
+    $('.modal-content').on('swipeleft', function () {
         if (popup_open && previous_item && document.documentElement.clientWidth <= 700) {
             PreviousItem();
         }
@@ -172,6 +142,17 @@ $(document).ready(function () {
         audio.currentTime = ((e.pageX - $(this).offset().left) / $(this).innerWidth()) * audio.duration;
     });
     
+    function SetModal (num) {
+        current_num = num;
+        current_data = item_data[num];
+        current_id = query.id = current_data.id;
+        UpdateQueries();
+        if (popup_open) {
+            ResetModal();
+        } else {
+            OpenModal();
+        }
+    }
     function ToggleAudio () {
         if (play) {
             play = false;
@@ -195,47 +176,45 @@ $(document).ready(function () {
             return '0:00';
         }
     }
-    function UpdatePopup () {
+    function UpdateModal () {
         current_id = item_data[current_num].id;
-        $('.modal-content-header-image').attr('src', 'img/musical-compositions/' + current_id + '/cover.jpg');
+        document.querySelector('.modal-content-header-image').setAttribute('src', 'img/musical-compositions/' + current_id + '/cover.jpg');
         $('.modal-content-header-image').prev('source').attr('srcset', 'img/musical-compositions/' + current_id + '/image.jpg');
         $('.modal-content-audio').html('<source src="audio/musical-compositions/' + current_id + '.mp3" type="audio/mp3" /><source src="audio/musical-compositions/' + current_id + '.ogg" type="audio/ogg" />');
         $('.modal-content-body').append('<h3>' + item_data[current_num].name + '</h3><small>' + item_data[current_num].date + '</small>' + '<h2>For ' + item_data[current_num].instrumentation + '</h2>');
-        if (item_data[current_num].credit) {
-            $('.modal-content-credit').text('Image by ' + item_data[current_num].credit + '.');
+        if (current_data.credit) {
+            $('.modal-content-credit').text('Image by ' + current_data.credit + '.');
         }
         $('.page-content-grid-item[data-num=' + current_num + ']').addClass('page-content-grid-item_focus');
-        $('.modal-content-header-close').focus();
         audio.load();
-        if (item_data[current_num].dark) {
-            $('.modal-content-header').addClass('white');
+        if (current_data.light) {
+            $('.modal-content-header').addClass('light');
         } else {
-            $('.modal-content-header').removeClass('white');
+            $('.modal-content-header').removeClass('light');
         }
         UpdateSocialLinks();
         $.get('text/musical-compositions/' + current_id + '.html', function (text) {
             $('.modal-content-body').append(text);
         });
     }
-    function ClearPopup () {
+    function ClearModal () {
         if (play) {
             ToggleAudio();
         }
         $('.modal-content-header-toolbar-icon_play').html('&#xf04b;');
         setTimeout(function () {
             $('.modal-content-header-toolbar-player-progress-played, .modal-content-header-toolbar-player-progress-loaded').css('width', 0);
-
         }, 1);
         $('.modal-content-body').empty();
         $('.modal-content-credit').empty();
         $('.page-content-grid-item[data-num!=' + current_num + ']').removeClass('page-content-grid-item_focus');
     }
-    function ResetPopup () {
+    function ResetModal () {
         CheckFirstLast();
         $('.modal-content').addClass('hide');
         setTimeout(function () {
-            ClearPopup();
-            UpdatePopup();
+            ClearModal();
+            UpdateModal();
         }, 501);
         setTimeout(function () {
             $('.modal-content').removeClass('hide');
@@ -257,40 +236,42 @@ $(document).ready(function () {
             $('.modal-arrow_right').removeClass('modal-arrow_disabled');
         }
     }
-    function OpenPopup () {
+    function OpenModal () {
         $('.modal').css('pointer-events', 'auto');
         CheckFirstLast();
-        UpdatePopup();
+        UpdateModal();
         popup_open = true;
-        setTimeout(function () {$('.modal').addClass('open')}, 80);
+        $('.modal-content-header-close').focus();
+        setTimeout(function () {$('.modal').addClass('open')}, 100);
     }
-    function ClosePopup () {
+    function CloseModal () {
         $('.page-content-grid-item[data-num=' + current_num + ']').focus();
         $('.modal').removeClass('open');
         setTimeout(function () {
-            ClearPopup();
+            ClearModal();
             $('.modal').css('pointer-events', 'none');
         }, 500);
+        query.id = '';
+        UpdateQueries();
         popup_open = false;
         current_num = null;
         current_id = null;
+        current_data = {};
         next_item = false;
         previous_item = false;
     }
     function NextItem () {
-        current_num -= 1;
-        query.id = item_data[current_num].id;
-        UpdateQueries();
-        ResetPopup();
+        if (next_item) {
+            SetModal(current_num - 1);
+        }
     }
     function PreviousItem () {
-        current_num += 1;
-        query.id = item_data[current_num].id;
-        UpdateQueries();
-        ResetPopup();
+        if (previous_item) {
+            SetModal(current_num + 1);
+        }
     }
     function UpdateSocialLinks () {
-        var current_name = encodeURI(item_data[current_num].name);
+        var current_name = encodeURI(current_data.name);
         $('.modal-content-header-toolbar-share-icon_email').attr('href', 'mailto:?body=' + current_url + '%3Fid%3D' + current_id + '&subject=' + current_name);
         $('.modal-content-header-toolbar-share-icon_facebook').attr('href', 'https://www.facebook.com/sharer/sharer.php?u=' + current_url + '%3Fid%3D' + current_id + '&t=' + current_name);
         $('.modal-content-header-toolbar-share-icon_google').attr('href', 'https://plus.google.com/share?url=' + current_url + '%3Fid%3D' + current_id);
@@ -313,49 +294,53 @@ $(document).ready(function () {
                 queryString += prop + '=' + query[prop];
             }
         }
-        window.history.replaceState('', 'Isaac Mailach - Musical Compositions' + (query.id ? ' - ' + item_data[item_num[query.id]].name : ''), window.location.pathname + queryString);
-        document.title = 'Isaac Mailach - Musical Compositions' + (query.id ? ' - ' + item_data[item_num[query.id]].name : '');
+        window.history.replaceState('', 'Isaac Mailach - Musical Compositions' + (query.id ? ' - ' + current_data.name : ''), window.location.pathname + queryString);
+        document.title = 'Isaac Mailach - Musical Compositions' + (query.id ? ' - ' + current_data.name : '');
     }
     function SearchItems (phrase) {
+        query.q = phrase;
+        UpdateQueries();
         for (var i = 0; i < item_data.length; i++) {
             var search_item = $('[data-num="' + i + '"]');
-            item_data[i].old_pos = search_item.position();
-            item_data[i].old_width = search_item.width();
-            if (!item_data[i].hidden) {
+            var search_item_data = item_data[i];
+            search_item_data.old_pos = search_item.position();
+            search_item_data.old_width = search_item.width();
+            if (!search_item_data.hidden) {
+                search_item_data.animate = true;
                 search_item.css({animation: '', transform: '', transition: ''});
-            }
-            if (!item_data[i].hidden) {
-                item_data[i].animate = true;
             } else {
-                item_data[i].animate = false;
+                search_item_data.animate = false;
             }
         }
         for (var i = 0; i < item_data.length; i++) {
             var search_item = $('[data-num="' + i + '"]');
+            var search_item_data = item_data[i];
             if (search_item.text().search(new RegExp(phrase, "i")) < 0) {
-                item_data[i].hidden = true;
-                search_item.css({'position': 'absolute', top: item_data[i].old_pos.top + 'px', left: item_data[i].old_pos.left + 'px', width: item_data[i].old_width + 'px'});
+                search_item_data.hidden = true;
+                search_item.css({'position': 'absolute', top: search_item_data.old_pos.top + 'px', left: search_item_data.old_pos.left + 'px', width: search_item_data.old_width + 'px'});
                 search_item.addClass('hide_search');
             } else {
-                item_data[i].hidden = false;
+                search_item_data.hidden = false;
                 search_item.css({'position': '', top: '', left: '', width: ''});
             }
         }
         for (var i = 0; i < item_data.length; i++) {
             var search_item = $('[data-num="' + i + '"]');
-            if (!item_data[i].hidden) {
-                item_data[i].new_width = search_item.width();
+            var search_item_data = item_data[i];
+            if (!search_item_data.hidden) {
+                search_item_data.new_width = search_item.width();
                 search_item.removeClass('hide_search');
-            }
-            if (!item_data[i].hidden && item_data[i].animate) {
-                item_data[i].new_pos = search_item.position();
-                search_item.css({transform: 'translate(' + (item_data[i].old_pos.left - item_data[i].new_pos.left) + 'px, ' + (item_data[i].old_pos.top - item_data[i].new_pos.top) + 'px) scale(' + (item_data[i].old_width / item_data[i].new_width) + ', 1)', transition: 'none'});
+                if (search_item_data.animate) {
+                    search_item_data.new_pos = search_item.position();
+                    search_item.css({transform: 'translate(' + (search_item_data.old_pos.left - search_item_data.new_pos.left) + 'px, ' + (search_item_data.old_pos.top - search_item_data.new_pos.top) + 'px) scale(' + (search_item_data.old_width / search_item_data.new_width) + ', 1)', transition: 'none'});
+                }
             }
         }
         setTimeout(function () {
         for (var i = 0; i < item_data.length; i++) {
             var search_item = $('[data-num="' + i + '"]');
-            if (!item_data[i].hidden) {
+            var search_item_data = item_data[i];
+            if (!search_item_data.hidden) {
                 search_item.css({animation: 'search_animate 0.5s cubic-bezier(0.4, 0, 0.2, 1)'});
             }
         }
@@ -363,16 +348,13 @@ $(document).ready(function () {
         setTimeout(function () {
             for (var i = 0; i < item_data.length; i++) {
                 var search_item = $('[data-num="' + i + '"]');
-                if (!item_data[i].hidden) {
+                var search_item_data = item_data[i];
+                if (!search_item_data.hidden) {
                     search_item.css({animation: '', transform: '', transition: ''});
                 }
             }
         }, 501);
     }
-    var SearchItems_debounce = _.debounce(function (query) {
-        UpdateQueries();
-        SearchItems(query);
-    }, 300);
     function CheckQueries () {
         var subString = window.location.search.substring(1).split('&');
         query = {id: '', q: ''};
@@ -381,12 +363,12 @@ $(document).ready(function () {
             query[vars[0]] = vars[1];
         }
         if (query.id) {
-            current_num = item_num[query.id];
-            OpenPopup();
+            SetModal(item_num[query.id]);
         }
         if (query.q) {
-            SearchItems(decodeURI(query.q));
-            $('.page-content-search-input').val(decodeURI(query.q));
+            var query_text = decodeURI(query.q);
+            SearchItems(query_text);
+            search_input.value = query_text;
         }
     }
 });
